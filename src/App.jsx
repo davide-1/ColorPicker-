c39c8486a2e704dae14772961fae85a911dd8ccd
Davide <davidelaplaca92@gmail.com>
2023-08-12 17:12:32 -0700

    add backgroung

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,58 @@
 import {useState} from "react";
+import "./index.css";
+
+
+// // Write your Color component here
+// const Color = ({color, setSelectedColor}) => {
+//   return <div className={color}
+//   onClick={() => setSelectedColor(color)}
+//   ></div>
+// }
+
+// const App = () => {
+//   const [selectedColor, setSelectedColor] = useState("");
+//   return (
+//     <div id="container">
+//       <div id="navbar">
+//         <div>Currently selected: </div>
+//         <div className={selectedColor}>{selectedColor}</div>
+//       </div>
+//       <div id="colors-list">
+//         <Color color="black" setSelectedColor={setSelectedColor}/>
+//         <Color color="violet" setSelectedColor={setSelectedColor}/>
+//         <Color color="blue" setSelectedColor={setSelectedColor}/>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
 
 
 
-const Color = ({ color, selectedColor, setSelectedColor }) => {
-  const isSelected = selectedColor === color;
-
-  return (
-    <div
-      className={`color-option ${color} ${isSelected ? "selected" : ""}`}
-      onClick={() => setSelectedColor(color)}
-    ></div>
-  );
-};
 
 const App = () => {
+  const Color = ({ color, selectedColor, setSelectedColor }) => {
+    const isSelected = selectedColor === color;
+  
+    return (
+      <div
+        className={`color-option ${color} ${isSelected ? "selected" : ""}`}
+        onClick={() => setSelectedColor(color)}
+      ></div>
+    );
+  };
   const [selectedColor, setSelectedColor] = useState("");
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className={`color-option ${selectedColor} selected`}></div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
         <Color
           color="black"
-          selectedColor={selectedColor}
+          selectedColor={selectedColor} 
           setSelectedColor={setSelectedColor}
         />
         <Color
